e9e363ff5250fbf3fcc12da2b11cfd17297f78c7
Joanne Santhosh <99355156+joannesanthosh@users.noreply.github.com>
2022-06-11 19:29:41 +0000

    fix format

--- main.cs
+++ main.cs
@@ -5,22 +5,24 @@
 
 using System;
 
-class Program {
-  public static void Main(string[] args) {
-    System.Random random = new System.Random();
-    int number = random.Next(1, 6);
-    int userGuess;
+class Program
+{
+     public static void Main(string[] args)
+    {
+       System.Random random = new System.Random();
+       int number = random.Next(1,6);
+       int userGuess;
 
-    // input
-    Console.WriteLine("Guess a number between 1-6");
-    userGuess = int.Parse(Console.ReadLine());
+      // input
+      Console.WriteLine("Guess a number between 1-6");
+      userGuess = int.Parse(Console.ReadLine());
 
-    // process
-    if (userGuess == number) {
-      Console.WriteLine("Correct answer!");
-    } else {
-      Console.WriteLine("Incorrect, try again!");
+      // process
+      if (userGuess == number) {
+        Console.WriteLine("Correct answer!");
+      } else {
+        Console.WriteLine("Incorrect, try again!");
+      }
+        Console.WriteLine("\nDone.");
     }
-    Console.WriteLine("\nDone.");
-  }
 }
